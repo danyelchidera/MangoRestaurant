592cddc140847a2352a8629a50e84e0cad5e7183
danyelchidera <danyelchidera1@gmail.com>
2022-08-04 00:59:48 +0100

    Add project files.

---
+++ Mango.Services.ProductAPI/ProductMaps.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI
+{
+    public class ProductMaps: Profile
+    {
+        public ProductMaps()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+        }
+    }
+}
---
+++ Mango.Services.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<ProductDto> CreateUpdateProduct(ProductDto productDto)
+        {
+            var product = _mapper.Map<Product>(productDto);
+            if(product.ProductId > 0)
+            {
+                _context.Update(product);
+            }
+            else
+            {
+                _context.Add(product);
+            }
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<ProductDto>(product);
+        }
+
+        public async Task<bool> DeleteProduct(int productId)
+        {
+            try
+            {
+                var product = _context.Products.FirstOrDefault(p => p.ProductId == productId);
+
+                if (product == null)
+                    return false;
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;   
+            }
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return _mapper.Map<List<ProductDto>>(products);
+        }
+
+        public  async Task<ProductDto> GetProductsById(int productId)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.ProductId.Equals(productId));
+            return _mapper.Map<ProductDto>(product);
+        }
+    }
+}
